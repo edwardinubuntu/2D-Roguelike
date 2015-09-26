57c18f564ad42c71c2ecb78c2c9bfa6d229fa25e
bryanchae <bryan450497129@gmail.com>
2015-09-26 12:03:33 +0800

    Player Script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
 	private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
 	private int level = 3;                                  //Current level number, expressed in game as "Day 1".
 	
 	//Awake is always called before any Start functions
@@ -39,7 +43,9 @@ public class GameManager : MonoBehaviour {
 		
 	}
 	
-	
+	public void GameOver (){
+		enabled = false;
+	}
 	
 	//Update is called every frame.
 	void Update()
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+
+
+	private Animator animator;
+	private int food;
+
+
+	// Use this for initialization
+	protected override void Start () {
+		animator = GetComponent<Animator> ();
+
+		food = GameManager.instance.playerFoodPoints;
+
+		base.Start ();
+	}
+
+	private void OnDisable()
+	{
+		GameManager.instance.playerFoodPoints = food;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
