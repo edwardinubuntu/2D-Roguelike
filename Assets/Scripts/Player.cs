27fbd8af25a1972b49ab45c77f227deadb809fe3
bryanchae <bryan450497129@gmail.com>
2015-10-03 10:54:39 +0800

    ADD I CANT MOVE FUNCTION!!!!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,6 @@ public class Player : MovingObject
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
-	
 	private Animator animator;
 	private int food;
 
@@ -48,6 +47,33 @@ public class Player : MovingObject
 	
 	}
 
+	protected abstract void OnCantMove <T> (T component)
+	{
+		Wall hitWall = component as Wall;
+		hitWall.DamageWall (wallDamage);
+		animator.SetTrigger ("playerChop");
+	}
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Exit") {
+			Invoke ("Restart", restartLevelDelay);
+			enabled = false;
+		} else if (other.tag == "Food") {
+			food += pointsPerFood;
+			other.gameObject.SetActive (false);
+		} else if (other.tag == "Soda") {
+			food+= pointsPerSoda;
+			other.gameObject.SetActive(false);
+		}
+
+	}
+
+	private void Restart ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
 	protected override void AttemptMove <T> (int xDir, int yDir)
 	{
 		food--;
@@ -58,6 +84,13 @@ public class Player : MovingObject
 		CheckIfGameOver ();
 
 		GameManager.instance.playersTurn = false;
+	}
+
+	private void LoseFood (int loss)
+	{
+		animator.SetTrigger ("playerHit");
+		food -= loss;
+		CheckIfGameOver ();
 	}
 
 	private void CheckIfGameOver ()
