51c43c55a9fb479d07a7deb786049a2f693386c0
Edward Chiang <edwardubuntu@gmail.com>
2015-10-03 11:29:42 +0800

    Formate code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,22 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MovingObject {
+public class Enemy : MovingObject
+{
 	public int playerDamage;
-
-
 	private Animator animator;
 	private Transform target;
 	private bool skipMove;
 	
-	protected override void Start () {
-	
+	protected override void Start ()
+	{
 		animator = GetComponent < Animator> ();
 		target = GameObject.FindGameObjectsWithTag ("Player").transform;
 		base.Start ();
 	}
 
-	protected override void AttemptMove <T> (int xDir,int yDir)
+	protected override void AttemptMove <T> (int xDir, int yDir)
 	{
 		if (skipMove) {
 			skipMove = false;
@@ -27,21 +26,21 @@ public class Enemy : MovingObject {
 		skipMove = true;
 	}
 
-	public void MoveEnemy()
+	public void MoveEnemy ()
 	{
 		int xdir = 0;
 		int ydir = 0;
 		if (Mathf.Abs (target.position.x - transform.position.x) < float .Epsilon) {
 			ydir = target.position.y > transform.position.y ? 1 : -1;
 		} else {
-			xdir = target.position.x> transform.position.x? 1:-1;
+			xdir = target.position.x > transform.position.x ? 1 : -1;
 		}
 		
-		AttemptMove<Player> (xdir,  ydir);
+		AttemptMove<Player> (xdir, ydir);
 	}
 
-	protected override void OnCantMove <T> (T component) {
-
+	protected override void OnCantMove <T> (T component)
+	{
 		Player hitPlayer = component as Player;
 		hitPlayer.LoseFood (playerDamage);
 	}
